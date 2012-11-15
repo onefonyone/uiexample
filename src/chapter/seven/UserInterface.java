7bf98758f2f2629eb710b33dda50a500995b4952
Gail Glasgow <gglasgow@mrcomputer.net>
2012-11-14 20:57:21 -0700

    added menu with 4 working menu items

--- src/chapter/seven/UserInterface.java
+++ src/chapter/seven/UserInterface.java
@@ -3,6 +3,10 @@ package chapter.seven;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class UserInterface extends Activity {
 
@@ -14,7 +18,32 @@ public class UserInterface extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_user_interface, menu);
-        return true;
+    	getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;    
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	LinearLayout bkgr = (LinearLayout)findViewById(R.id.uilayout);
+    	ImageView image = (ImageView)findViewById(R.id.imageButton1);
+    	
+    	switch (item.getItemId()) {
+	    	case R.id.buttonone:
+	    		image.setImageResource(R.drawable.image1);
+	    		break;
+	    	case R.id.buttontwo:
+	    		image.setImageResource(R.drawable.image2);
+	    		break;
+	    	case R.id.buttonthree:
+	    		bkgr.setBackgroundResource(R.color.background2);
+	    		break;
+	    	case R.id.buttonfour:
+	    		bkgr.setBackgroundResource(R.color.background1);
+	    		break;
+	    	case R.id.buttonfive:
+	    		// the alert code for next section goes here!
+	    		break;
+    	}
+    	return true;
     }
 }
